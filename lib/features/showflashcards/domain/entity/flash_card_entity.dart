39975933ff2ff31b9758b1e4eed29102ccbcafa6
Shahab Golpaiegan Asl <shahab@shahabs-MacBook-Pro.local>
2023-04-23 04:48:08 +0330

    sample structure added

---
+++ lib/features/showflashcards/domain/entity/flash_card_entity.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+class FlashCardEntity extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
